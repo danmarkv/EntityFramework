c0e975f26fa8f0b3fef097df47bf767c8865e27f
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-03-18 11:01:06 +0800

    adding external enums to the data type of Level

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace DbFirst
 {
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
     class Program
     {
         static void Main()
         {
-            var dbContext = new PlutoDbContext();
-            var courses = dbContext.GetCourses();
-            foreach(var c in courses)
-                Console.WriteLine(c.Title);
+            var course = new Course();
+            course.Level = Level.Advanced; // 1
         }
     }
 }
